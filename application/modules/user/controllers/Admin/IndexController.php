ee7abb0ee3dd8e288c8e15e52b001c8410db42bd
Serial Graphics <peter@serial-graphics.be>
2012-02-21 12:16:19 +0100

    Added admin menu

---
+++ application/modules/user/controllers/Admin/IndexController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @category User_Admin
+ * @package  Controller
+ * @author   Peter Decuyper <sgrame@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html MIT License
+ * @link     https://github.com/sgrame/openSGrame
+ * @filesource
+ */
+
+
+/**
+ * User_IndexController
+ *
+ * Login controller
+ *
+ * @category User_Admin
+ * @package  Controller
+ * @author   Peter Decuyper <sgrame@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html MIT License
+ * @link     https://github.com/sgrame/openSGrame
+ */
+class User_Admin_IndexController extends Zend_Controller_Action
+{
+    /**
+     * Redirect to login if no access
+     */
+    public function init()
+    {
+        
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
+}
+
