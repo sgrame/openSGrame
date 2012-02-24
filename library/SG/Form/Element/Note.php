940244936ffd3cac7b19e1ff6013477873f99b0d
Serial Graphics <peter@serial-graphics.be>
2012-02-24 13:23:58 +0100

    Added "Note" form element

---
+++ library/SG/Form/Element/Note.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @category SG
+ * @package  Form
+ * @author   Peter Decuyper <sgrame@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html MIT License
+ * @link     https://github.com/sgrame/openSGrame
+ * @filesource
+ */
+
+
+/**
+ * SG_Form_Element_Note
+ *
+ * Form element to output plain text instead of input field
+ *
+ * @category SG
+ * @package  Form
+ * @author   Peter Decuyper <sgrame@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html MIT License
+ * @link     https://github.com/sgrame/openSGrame
+ */
+class SG_Form_Element_Note extends Zend_Form_Element_Xhtml 
+{ 
+    public $helper = 'formNote'; 
+}
