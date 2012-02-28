ac69c6c8121c6e6e8913e5b25f71a415cf07a5ba
Serial Graphics <peter@serial-graphics.be>
2012-02-28 23:40:22 +0100

    Started adding the permissions administration

---
+++ application/modules/user/controllers/Admin/PermissionsController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @category User_Admin
+ * @package  Controller
+ * @author   Peter Decuyper <sgrame@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html MIT License
+ * @link     https://github.com/sgrame/openSGrame
+ * @filesource
+ */
+
+
+/**
+ * User_Admin_PermissionsController
+ *
+ * Users configuration
+ *
+ * @category User_Admin
+ * @package  Controller
+ * @author   Peter Decuyper <sgrame@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.html MIT License
+ * @link     https://github.com/sgrame/openSGrame
+ */
+class User_Admin_PermissionsController extends Zend_Controller_Action
+{
+    /**
+     * Model
+     * 
+     * @var User_Model_Permission
+     */
+    protected $_model;
+    
+    /**
+     * Messenger
+     * 
+     * @var TB_Controller_Action_Helper_Messenger
+     */
+    protected $_messenger;
+  
+    /**
+     * Init this controller
+     */
+    public function init()
+    {
+       $this->_model = new User_Model_Permission();
+       $this->_messenger = $this->_helper->getHelper('Messenger');
+       
+       $this->view->layout()->title = $this->view->t('Manage permissions');
+       
+       // set the subnavigation
+       $this->view->layout()->subnavId = 'sub-user-admin';
+    }
+
+    public function indexAction()
+    {
+    }
+
+
+}
+
