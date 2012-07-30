b98c13dbf03ad3aee0036ba9ae0bb432a9afc2ea
Peter Decuyper <peter@serial-graphics.be>
2012-07-31 00:25:06 +0200

    Fixed exception test

--- openSGrame/library/SG/Rule/Parser/Comparison/NotEqual.php
+++ openSGrame/library/SG/Rule/Parser/Comparison/NotEqual.php
@@ -39,72 +39,14 @@ class SG_Rule_Parser_Comparison_NotEqual extends SG_Rule_Parser_Abstract
             throw new SG_Rule_Parser_Exception('Unable to parse string.');
         }
         
-
-        $parts  = $this->match($string);
+        $parts = $patterns->split('!=', $string);
+        if (2 !== count($parts)) {
+            throw new SG_Rule_Parser_Exception('Unable to parse string.');
+        }
         
         $left  = $patterns->parse($parts[0]);
         $right = $patterns->parse($parts[1]);
         
         return new SG_Rule_Comparison_NotEqual($left, $right);
-    }
-    
-    /**
-     * Function to split the string in parts
-     * 
-     * @param string $string
-     * 
-     * @return array 
-     */
-    public function match($string) 
-    {
-        $result = $this->_split($string);
-        if (2 !== count($result)) {
-            throw new SG_Rule_Parser_Exception('Unable to parse string.');
-        }
-        
-        return $result;
-    }
-    
-    /**
-     * Method to split the string
-     * 
-     * @param string $string
-     * 
-     * @return array 
-     */
-    protected function _split($string)
-    {
-        $parts  = array();
-        $part   = NULL;
-        $level  = 0; 
-        for($i = 0; $i < strlen($string); $i++) {
-            $sub = substr($string, $i, 1);
-            
-            if ($sub === '(') {
-                $part .= $sub;
-                $level++;
-                continue;
-            }
-            if ($sub === ')') {
-                $part .= $sub;
-                $level--;
-                continue;
-            }
-            if ($level > 0) {
-                $part .= $sub;
-                continue;
-            }
-            if (substr($string, $i, 2) === '!=') {
-                $parts[] = $part;
-                $part = NULL;
-                $i++;
-                continue;
-            }
-
-            $part .= $sub;
-        }
-        $parts[] = $part;
-        
-        return $parts;
     }
 }
--- tests/library/SG/Rule/Parser/Comparison/NotEqualTest.php
+++ tests/library/SG/Rule/Parser/Comparison/NotEqualTest.php
@@ -6,42 +6,6 @@
 class SG_Rule_Parser_Comparison_NotEqualTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * Test the splitter
-     */
-    public function testMatch()
-    {
-        $patterns = new SG_Rule_Parser_Patterns();
-        $parser    = new SG_Rule_Parser_Comparison_NotEqual();
-        
-        $string = '50!=20';
-        $result = $parser->match($string);
-        $this->assertEquals(2, count($result));
-        $this->assertEquals('50', $result[0]);
-        $this->assertEquals('20', $result[1]);
-        
-        $string = '50!=AND(20=50;30=60;40=70)';
-        $result = $parser->match($string);
-        $this->assertEquals(2, count($result));
-        $this->assertEquals('50', $result[0]);
-        $this->assertEquals('AND(20=50;30=60;40=70)', $result[1]);
-        
-        $string = '50=AND!=A(20=50;30=60;40=70)';
-        $result = $parser->match($string);
-        $this->assertEquals(2, count($result));
-        $this->assertEquals('50=AND', $result[0]);
-        $this->assertEquals('A(20=50;30=60;40=70)', $result[1]);
-        
-        try {
-            $string = '50=20';
-            $parser->match($string);
-            $this->fail('The string is wrongly matched');
-        }
-        catch(Exception $e) {
-            
-        }
-    }
-    
-    /**
      * Test the parser 
      */
     public function testParse()
