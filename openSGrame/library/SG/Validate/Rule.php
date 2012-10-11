fc3517f21c9db21355de308ad84b01c1bb51d159
Peter Decuyper <peter@serial-graphics.be>
2012-10-11 22:56:23 +0200

    Finished SG_Rule validator

--- openSGrame/library/SG/Validate/Rule.php
+++ openSGrame/library/SG/Validate/Rule.php
@@ -10,10 +10,9 @@
 
 
 /**
- * SG_Validate_Name
+ * SG_Validate_Rule
  *
- * Validates a string to be a name
- * Extends the Zend_Validate_Alpha and set the allow whitespace to true
+ * Validates a string to be parsable as a SG_Rule
  *
  * @category SG
  * @package  Validate
@@ -21,14 +20,121 @@
  * @license  http://www.opensource.org/licenses/mit-license.html MIT License
  * @link     https://github.com/sgrame/openSGrame
  */
-class SG_Validate_Name extends Zend_Validate_Alpha
+class SG_Validate_Rule extends SG_Validate_Abstract
 {
     /**
-     * Class constructor
+     * Error messages keys
+     * 
+     * @var string
      */
-     public function __construct($allowWhiteSpace = true)
+    const INVALID_RULE    = 'error_not_a_valid_rule';
+    
+    /**
+     * Error messages
+     * 
+     * @var array
+     */
+    protected $_messageTemplates = array(
+        self::INVALID_RULE    => "'%value%' is not a valid rule",
+    );
+    
+    /**
+     * Parser
+     * 
+     * @var SG_Rule_Parser_Rule
+     */
+    protected $_parser;
+    
+    /**
+     * Patterns
+     * 
+     * @var SG_Rule_Parser_Patterns
+     */
+    protected $_patterns;
+    
+    /**
+     * Set the parser
+     * 
+     * @param SG_Rule_Parser
+     * 
+     * @return SG_Validate_Rule
+     */
+    public function setParser(SG_Rule_Parser_Rule $parser)
     {
-        parent::__construct($allowWhiteSpace);
-        self::$_filter = new SG_Filter_Name();
+        $this->_parser = $parser;
+        return $this;
+    }
+    
+    /**
+     * Get the parser
+     * 
+     * @param void
+     * 
+     * @return SG_Rule_Parser
+     */
+    public function getParser()
+    {
+        return $this->_parser;
+    }
+    
+    /**
+     * Set the patterns
+     * 
+     * @param SG_Rule_Parser_Patterns $patterns
+     * 
+     * @return SG_Validate_Rule
+     */
+    public function setPatterns(SG_Rule_Parser_Patterns $patterns)
+    {
+        $this->_patterns = $patterns;
+        return $this;
+    }
+    
+    /**
+     * Get the patterns
+     * 
+     * @param void
+     * 
+     * @return SG_Rule_Parser_Patterns
+     */
+    public function getPatterns()
+    {
+        return $this->_patterns;
+    }
+     
+    /**
+     * Defined by Zend_Validate_Interface
+     *
+     * Returns true if the given value contains a string that can be parsed by 
+     * the SG_Rule_Parser 
+     *
+     * @param  string $value
+     * 
+     * @return bool
+     */
+    public function isValid($value)
+    {
+        $valueString = (string) $value;
+        $this->_setValue($valueString);
+        
+        $parser = $this->getParser();
+        $patterns = $this->getPatterns();
+        
+        if(empty($parser)) {
+            throw new Zend_Validate_Exception("Can't validate rules without parser");
+        }
+        if(empty($patterns)) {
+            throw new Zend_Validate_Exception("Can't validate rules without patterns");
+        }
+            
+        try {
+            $parser->parse($value, $patterns);
+        }
+        catch(Exception $e) {
+            $this->_error(self::INVALID_RULE);
+            return false;
+        }
+        
+        return true;
     }
 }
