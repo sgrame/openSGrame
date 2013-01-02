296466d79c48b91d00c5c4bd400c482f73765959
Peter Decuyper <peter@serial-graphics.be>
2013-01-02 18:26:08 +0100

    Added user config form

--- openSGrame/application/modules/user/controllers/Admin/ConfigController.php
+++ openSGrame/application/modules/user/controllers/Admin/ConfigController.php
@@ -31,24 +31,53 @@ class User_Admin_ConfigController extends SG_Controller_Action
     /**
      * Model
      * 
-     * @var User_Model_Group
+     * @var User_Model_Config
      */
     protected $_model;
-  
+    
+    /**
+     * Messenger
+     * 
+     * @var TB_Controller_Action_Helper_Messenger
+     */
+    protected $_messenger;
+
+
     /**
      * Init this controller
      */
     public function init()
     {
-       $this->_model = new User_Model_Group();
-       $this->view->layout()->title = $this->view->t('Users configuration');
+       $this->_model = new User_Model_Config();
+       $this->_messenger = $this->_helper->getHelper('Messenger');
        
-       // set the subnavigation
+       $this->view->layout()->title = $this->view->t('User configuration');
        $this->view->layout()->subnavId = 'sub-user-admin';
     }
 
     public function indexAction()
     {
+        $form = $this->_model->getForm();
+        $this->view->form = $form;
+        
+        if (!$this->_request->isPost()) {
+            return;
+        }
+        
+        if (!$form->isValid($this->_request->getParams())) {
+            return;
+        }
+        
+        $this->_model->saveForm($form);
+        $this->_messenger->addSuccess($this->view->t('Configuration saved.'));
+        
+        // redirect after save
+        $this->_gotoDestination();
+        $this->_redirect($this->view->url(array(
+            'module'     => 'user',
+            'controller' => 'config',
+            'action'     => 'index',
+        ), 'admin', true));
     }
 
 
--- openSGrame/application/modules/user/forms/Config.php
+++ openSGrame/application/modules/user/forms/Config.php
@@ -20,7 +20,7 @@
  * @license  http://www.opensource.org/licenses/mit-license.html MIT License
  * @link     https://github.com/sgrame/openSGrame
  */
-class User_Form_User extends SG_Form
+class User_Form_Config extends SG_Form
 {
     /**
      * Configure user form.
@@ -30,165 +30,60 @@ class User_Form_User extends SG_Form
     public function init()
     {
         // form config
-        $this->setAttrib('id', 'user-form');
-
-        // create elements
-        $username  = new Zend_Form_Element_Text('username');
-        $username  ->setLabel('Username')
-                   ->setRequired(true)
-                   ->setAttrib('autocomplete', 'off');
-                   
-        // password
-        $validator = new Zend_Validate_StringLength(6, 32);
-        $this->addElement('password', 'user_password', array(
-            'label'      => 'Password',
-            'required'   => true,
-            'maxlength'     => 128,
-            'validators' => array($validator),
-        ));
-        $this->addElement('password', 'user_password_confirm', array(
-            'label'      => 'Repeat password',
-            'required'   => true,
-            'maxlength'     => 128,
-            'description'=> 'Password must contain at least 6 characters.',
-            'validators' => array(
-                array(
-                    'validator' => 'FormIdentical',
-                    'options'    => array(
-                        'user_password'
-                    )
-                ),
+        $this->setAttrib('id', __CLASS__);
+                
+        // password length
+        $minPasswordRange = range(4, 32);
+        $minPasswordLength = new Zend_Form_Element_Select('minimum_password_length');
+        $minPasswordLength 
+            ->setLabel('Min. password length')
+            ->setDescription(
+                'The longer the required length, the harder to force-break passwords.'
             )
-        ));
+            ->addMultiOptions($minPasswordRange);
         
-        $firstname  = new Zend_Form_Element_Text('firstname');
-        $firstname ->setLabel('Firstname')
-                   ->setRequired(true)
-                   ->setAttrib('autocomplete', 'off');
-        $lastname  = new Zend_Form_Element_Text('lastname');
-        $lastname  ->setLabel('Lastname')
-                   ->setRequired(true)
-                   ->setAttrib('autocomplete', 'off');
-        
-        $this->addElement('email', 'email', array(
-            'label'      => 'Email',
-            'required'   => true,
-            'attribs'    => array(
-                'autocomplete' => 'off',
-            ),
-        ));
-        
-        $groups    = new User_Form_Element_SelectGroups('groups');
-        $groups    ->setLabel('Group');
-        
-        $roles     = new User_Form_Element_MultiCheckboxRoles('roles');
-        $roles     ->setLabel('Role(s)'); 
-        
-        $status    = new User_Form_Element_RadioStatus('status');
-        $status    ->setLabel('Status');
-        $status    ->setValue(1);
-        $status    ->setDescription(
-            'Blocking an user will prevent him from logging in to the platform.'
+        // number of login attempts
+        $maxLoginRange     = array_merge(
+            array(0 => 'Unlimitted'), 
+            range(1, 15)
         );
+        $maxLoginAttempts  = new Zend_Form_Element_Select('maximum_login_attempts');
+        $maxLoginAttempts  
+            ->setLabel('Max. login attempts')
+            ->setDescription(
+                'Users will be locked out if they entered several wrong passwords. This will block forced log-in attempts.'
+             )
+            ->addMultiOptions($maxLoginRange);
         
+        // buttons
         $submit    = new Zend_Form_Element_Submit('submit');
         $submit    ->setLabel('Save');
         
-        $cancel    = new Zend_Form_Element_Submit('cancel');
-        $cancel    ->setLabel('Cancel');
-
 
         // add elements
         $this->addElements(array(
-            $username, 
-            $firstname,
-            $lastname,
-            $groups,
-            $roles,
-            $status,
+            $minPasswordLength, 
+            $maxLoginAttempts,
             $submit,
-            $cancel,
         ));
         
         // Login group
         $this->addDisplayGroup(
             array(
-                'username', 
-                'user_password', 
-                'user_password_confirm'
+                'minimum_password_length', 
+                'maximum_login_attempts', 
             ),
             'login'
         );
-        $this->getDisplayGroup('login')->setLegend('User login');
-        
-        // User details group
-        $this->addDisplayGroup(
-            array(
-                'firstname', 
-                'lastname', 
-                'email', 
-                'roles', 
-                'groups', 
-                'status'
-            ),
-            'user'
-        );
-        $this->getDisplayGroup('user')->setLegend('User details');
-        
+        $this->getDisplayGroup('login')->setLegend('Login settings');
         
         // button group
         $this->addButtonGroup(
-            array('submit', 'cancel'),
+            array('submit'),
             'submit'
         );
         
-        $this->addElement('hash', 'login_csrf', array('salt' => 'user-form'));
-        $this->addElement('hidden', 'user_id');
-    }
-
-    /**
-     * Validate the form
-     *
-     * @param  array $data
-     * @return boolean
-     */
-    public function isValid($data)
-    {
-        $isValid = parent::isValid($data);
-        
-        if(!$isValid) {
-            return;
-        }
-        
-        // user id
-        $userId = $this->getValue('user_id');
-        
-        $model = new User_Model_DbTable_User();
-        
-        // check username exists
-        if($model->usernameExists($this->getValue('username'), $userId)) {
-            $isValid = false;
-            $this->getElement('username')->addError(
-                'Username already in use for other user'
-            );
-            $this->markAsError();
-        }
-        
-        // check email exists
-        $model = new User_Model_DbTable_User();
-        if($model->emailExists($this->getValue('email'), $userId)) {
-            $isValid = false;
-            $this->getElement('email')->addError(
-                'Email already in use for other user'
-            );
-            $this->markAsError();
-        }
-        
-        if(!$isValid) {
-            $this->buildErrorDecorators();
-        }
-        
-        return $isValid;
+        $this->addHash();
     }
 }
 
